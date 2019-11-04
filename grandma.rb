374235438320dcb13ceb236b2edee9247b5b2bb2
Nethelbert <gilblasse@gmail.com>
2019-11-04 13:50:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,8 +3,10 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     if phrase == "I LOVE YOU GRANDMA!"
-    elsif phrase == "I LOVE YOU GRANDMA!"
-    # she can hear
+      puts ""
+    else
+      puts "NO, NOT SINCE 1938!"
+    end
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
